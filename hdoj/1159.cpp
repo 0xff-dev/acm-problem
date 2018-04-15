d5d7868fae7bc21e2d5b8bfab1391eda97ae7234
stevenshuang <stevenshuang521@gmail.com>
2018-04-15 21:40:54 +0800

    solve hd 1159 longest common sub

---
+++ hdoj/1159.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+/*最长公共子序列*/
+int dp[1001][1001];
+char _str1[1001], _str2[1001];
+
+int main(){
+    string s1, s2;
+    while( cin >> s1 ){
+        cin >> s2;
+        for(int i=1; i<=s1.size(); i++)
+            _str1[i]=s1[i-1];
+        for(int i=1; i<=s2.size(); i++)
+            _str2[i]=s2[i-1];
+
+        memset(dp, 0, sizeof(dp));
+        int result=0;
+        for( int i=1; i<=s1.size(); i++ ){
+            for( int j=1; j<=s2.size(); j++ ){
+                if( _str1[i]==_str2[j] ){
+                    dp[j][i]=dp[j-1][i-1]+1;
+                }
+                else{
+                    dp[j][i]=max(dp[j-1][i], dp[j][i-1]);
+                }
+                if( result<dp[j][i] )
+                    result=dp[j][i];
+            }
+        }
+        cout << result << endl;
+    }
+}
