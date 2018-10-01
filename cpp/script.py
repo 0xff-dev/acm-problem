344fa83a9f03426b6e9f75cb2a169036175dcf3b
stevenshuang <stevenshuang521@gmail.com>
2018-10-01 21:17:31 +0800

    generate data

---
+++ cpp/script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from functools import 
+with open('./data.txt', 'w') as fp:
+    for i in range(1, 10):
+        data = ' '.join(str(i) for j in range(i))
+        fp.write(data+'\n')
