40dff54585160145bdc05f8cbf7e529e0c8ea38c
stevenshuang <stevenshuang521@gmail.com>
2018-11-07 00:03:30 +0800

    del script

--- cpp/script.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-
-with open('./data.txt', 'w') as fp:
-    for i in range(1, 10):
-        data = ' '.join(str(i) for j in range(i))
-        fp.write(data+'\n')
