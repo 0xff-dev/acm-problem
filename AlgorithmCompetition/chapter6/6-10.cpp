2d1f5aac0f56b4a4b13f4e90fc0aa2c9deb4bb92
stevenshuang <stevenshuang521@gmail.com>
2019-04-15 16:33:34 +0800

    6-10 下落的叶子，考虑有个左右位置关系，采用数组

---
+++ AlgorithmCompetition/chapter6/6-10.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+const int maxn = 100001;
+int sum[maxn];
+
+void build(int pos) {
+    int v; cin >> v;
+    if(v == -1) return;
+    sum[pos] += v;
+    build(pos-1), build(pos+1);    
+}
+
+bool init() {
+    int root; cin >> root;
+    if(root == -1) return false;
+    memset(sum, 0, sizeof(sum));
+    int pos = maxn/2;
+    sum[pos] = root;
+    build(pos-1), build(pos+1);
+}
+
+int main() {
+    while(init()) {
+        int index = 0;
+        while(sum[index] == 0) index++;
+        printf("Case: \n");
+        while(sum[index] != 0) {
+            cout << sum[index++] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
