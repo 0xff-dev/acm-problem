aa79d77d17e294c18849d0254bbea9bd960dc44b
stevenshuang <stevenshuang521@gmail.com>
2019-04-15 14:23:01 +0800

    类似树的高度的解法，两边递归

---
+++ AlgorithmCompetition/chapter6/6-9.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+// 左右子树相等的基础之上， 计算当前节点.
+bool solve(int& w) {
+    int w1, d1, w2, d2;
+    cin >> w1 >> d1 >> w2 >> d2;
+    bool r1, r2;
+    // 当左子树不为空的时候，
+    if(!w1) r1 = solve(w1);
+    if(!w2) r2 = solve(w2);
+    return r1 && r2 && (w1*d1 == w2*d2);
+}
+
+int main() {
+    int T;
+    int res;
+    cin >> T;
+    while(T--){
+        if(solve(res)) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
